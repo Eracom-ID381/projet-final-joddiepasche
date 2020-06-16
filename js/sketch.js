3ecc7603bbd712a6e22facd3b15cf3014acf08e5
joddiepasche <joddie.pasche@gmail.com>
2020-06-16 16:09:47 +0200

    ajout image etc.

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,30 @@
+let mobilenet;
+let crocodile;
+
+function modelReady() {
+    console.log('Model is ready!')
+    mobilenet.predict(crocodile, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        let label = results[0].className;
+        fill(0);
+        textSize(64);
+        text(label, 10, height - 100);
+    }
+}
+
+function imageReady() {
+    image(crocodile, 0, 0, width, height);
+}
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(640, 480);
+    crocodile = createImg('../images/crocodile.jpg');
     background(0);
-}
-
-function draw() {
-
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
 }
